fe1f7981e3b170411ae6cd01c4d719eda1793d0f
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-08-11 13:41:29 -0600

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Benjamin Rojo on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
